473f357d221975a87b4dd33700aada96ab9a6ac9
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-03-06 17:13:06 -0500

    Add critic scores to the game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,6 +13,7 @@ export interface Game {
   background_image: string;
   // Thats how it was deifined on the endpoint
   parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
 export interface GamesResponse {
