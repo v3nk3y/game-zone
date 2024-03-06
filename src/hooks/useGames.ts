e55f976f11c535c4f423c0f95bc63db1d219821a
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-03-06 16:27:32 -0500

    Refactoring file format

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,33 +3,33 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
 export interface Game {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 export interface GamesResponse {
-    count: number;
-    results: Game[];
+  count: number;
+  results: Game[];
 }
 
 const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-  
-    useEffect(() => {
-        const controller = new AbortController();
-        apiClient
-        .get<GamesResponse>("/games", {signal: controller.signal})
-        .then((res) => setGames(res.data.results))
-        .catch((err) => {
-            if(err instanceof CanceledError) return;
-            setError(err.message)
-        });
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
 
-        return () => controller.abort();
-    }, []);
-    
-    return {games, error, setGames, setError}
-}
+  useEffect(() => {
+    const controller = new AbortController();
+    apiClient
+      .get<GamesResponse>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error, setGames, setError };
+};
 
 export default useGames;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: 'c017353d2520452d9593caac4b20b856'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "c017353d2520452d9593caac4b20b856",
+  },
+});
