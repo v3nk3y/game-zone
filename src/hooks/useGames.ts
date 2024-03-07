868319bf41ef93be6206843038fac21e29630c34
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-03-07 15:30:15 -0500

    Sort games by order selected

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -22,6 +22,7 @@ const useGames = (gameQuery: GameQuery | null) =>
       params: {
         genres: gameQuery?.genre?.id,
         platforms: gameQuery?.platform?.id,
+        ordering: gameQuery?.sortOrder,
       },
     },
     [gameQuery]
