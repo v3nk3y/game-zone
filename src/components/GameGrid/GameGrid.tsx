8f9f7e601e892cc312bb1313f8f2cb2478780084
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-03-07 16:44:29 -0500

    Cleanup refactor game cards

--- src/components/GameGrid/GameGrid.tsx
+++ src/components/GameGrid/GameGrid.tsx
@@ -16,8 +16,8 @@ const GameGrid = ({ gameQuery }: Props) => {
     <>
       {error && <Text color="red">{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing="10px"
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={5}
         padding="10px"
       >
         {isLoading &&
