15b285df557cf149b7fd2c384d91f59fe5d3392a
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-03-07 16:12:19 -0500

    Search functionality added

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,16 +1,29 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input
-        placeholder="Search Games..."
-        variant="filled"
-        borderRadius={30}
-      ></Input>
-    </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref) onSearch(ref.current?.value || "");
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          placeholder="Search Games..."
+          variant="filled"
+          borderRadius={30}
+        ></Input>
+      </InputGroup>
+    </form>
   );
 };
 
