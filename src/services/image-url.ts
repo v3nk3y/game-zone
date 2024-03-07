af4100a35a0bfee4131fab7a027c935e46e7ef08
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-03-07 15:34:20 -0500

    Handle no game image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
+import noImage from "../assets/no-image.webp";
+
 // Util service for getting smaller image from the API by cropping it to 600x400
 const getCroppedImage = (url: string) => {
-  return url ? url.replace("media/games", "media/crop/600/400/games") : "";
+  return url ? url.replace("media/games", "media/crop/600/400/games") : noImage;
 };
 
 export default getCroppedImage;
