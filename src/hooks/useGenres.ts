96f020217af49f6e880d1b5b9b95d041ea540a68
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-03-07 17:14:46 -0500

    Example usage of static data for genrelist

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -6,6 +6,10 @@ export interface Genre {
   image_background: string;
 }
 
+// Getting data from the API call
 const useGenres = () => useData<Genre>("/genres");
+
+// Getting data from the local data object
+// const useGenres = () => ({ data: null, isLoading: false, error: null });
 
 export default useGenres;
