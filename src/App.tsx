edea197d201d2b56917c49189252f612d8d36cbd
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-03-08 12:02:42 -0500

    Refactor remove duplicate interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import GenreList from "./components/GenreList/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector/PlatformSelector";
-import { Platform } from "./hooks/useGames";
+import { Platform } from "./hooks/usePlatforms";
 import SortSelector from "./components/SortSelector/SortSelector";
 import GameHeading from "./components/GameHeading/GameHeading";
 
