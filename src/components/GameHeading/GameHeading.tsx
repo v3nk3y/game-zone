1e308aeb4ff277fcf7f9985dbb9a85e89ea0e7ae
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-03-08 15:19:02 -0500

    Refactor: creted lookup hooks for finding the genre, platform based on id

--- src/components/GameHeading/GameHeading.tsx
+++ src/components/GameHeading/GameHeading.tsx
@@ -1,14 +1,13 @@
 import { Heading } from "@chakra-ui/react";
 import { GameQuery } from "../../App";
-import useGenres from "../../hooks/useGenres";
+import useGenre from "../../hooks/useGenre";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const { data: genres } = useGenres();
-  const genre = genres.results.find((genre) => genre.id === gameQuery.genreId);
+  const genre = useGenre(gameQuery.genreId);
   const heading = `${genre?.name || ""} Games`;
   return (
     <Heading as="h1" marginY={5} fontSize="5xl">
