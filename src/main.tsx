d06d1845de6587db6ca34388adbe0fbf666dfb9d
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-03-09 16:30:40 -0500

    Setting up Routing for home and game details page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import App from "./App.tsx";
-import theme from "./theme.ts";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import router from "./routing/routes.tsx";
+import theme from "./theme.ts";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,7 +22,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <App />
+        <RouterProvider router={router} />
         <ReactQueryDevtools />
       </ChakraProvider>
     </QueryClientProvider>
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,39 @@
+import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import GameGrid from "../components/GameGrid/GameGrid";
+import GameHeading from "../components/GameHeading/GameHeading";
+import GenreList from "../components/GenreList/GenreList";
+import PlatformSelector from "../components/PlatformSelector/PlatformSelector";
+import SortSelector from "../components/SortSelector/SortSelector";
+
+const HomePage = () => {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"main"`,
+        lg: `"aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "250px 1fr",
+      }}
+    >
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        <Box paddingLeft={2}>
+          <GameHeading />
+          <HStack spacing={5} marginBottom={5}>
+            <PlatformSelector />
+            <SortSelector />
+          </HStack>
+        </Box>
+        <GameGrid />
+      </GridItem>
+    </Grid>
+  );
+};
+
+export default HomePage;
