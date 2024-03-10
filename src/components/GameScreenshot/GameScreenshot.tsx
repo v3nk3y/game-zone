9b60cc435a600ce3ffcb68b522afff7efe77f196
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-03-09 19:11:34 -0500

    Add Game Screenshots component on details page

---
+++ src/components/GameScreenshot/GameScreenshot.tsx
@@ -0,0 +1,21 @@
+import { Image, SimpleGrid, Spinner } from "@chakra-ui/react";
+import useGameScreenshots from "../../hooks/useGameScreenshots";
+
+interface Props {
+  gameId: number;
+}
+const GameScreenshot = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useGameScreenshots(gameId);
+  console.log(data);
+  if (isLoading) return <Spinner />;
+  if (error) throw error;
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+      {data?.results.map((file) => (
+        <Image key={file.id} src={file.image} />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default GameScreenshot;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,6 +4,7 @@ import ExpandableText from "../components/ExpandableText/ExpandableText";
 import GameAttributes from "../components/GameAttributes/GameAttributes";
 import GameTrailer from "../components/GameTrailer/GameTrailer";
 import useGame from "../hooks/useGame";
+import GameScreenshot from "../components/GameScreenshot/GameScreenshot";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -18,6 +19,7 @@ const GameDetailPage = () => {
         <ExpandableText>{game.description_raw}</ExpandableText>
         <GameAttributes game={game} />
         <GameTrailer gameId={game.id} />
+        <GameScreenshot gameId={game.id} />
       </Box>
     </>
   );
